cbcadeadbc04f830e7ead208141347156928bb9e
Ali Hussain Khan <alihussainkhan24@gmail.com>
2024-02-14 01:51:29 +0500

    updated maths

--- ball.js
+++ ball.js
@@ -11,13 +11,13 @@ var color = getRandomColor();
 // Sir, I need your input here, random number generator sometimes
 // generates a number that gets the ball stuck on top or on the sides, how can we avoid that
 
-let x = Math.floor(Math.random() * canva.width);
-let y = Math.floor(Math.random() * canva.height);
+let x = Math.floor(Math.random() * Math.random() * (1200 - 200) + 200);
+let y = Math.floor(Math.random() * Math.random() * (500 - 200) + 200);
 console.log(x, y);
 let FPS = 60;
 let radius = 20;
-let xspeed = 3;
-let yspeed = 6;
+let xspeed = Math.random() * Math.random() * (5 - 3) + 3;
+let yspeed = Math.random() * Math.random() * (6 - 2) + 2;
 
 function clear() {
   ctx.clearRect(0, 0, canva.width, canva.height);
@@ -42,18 +42,23 @@ function animate() {
   ctx.fill();
 
   if (x + radius >= canva.width) {
+    // x = canva.width - radius;
     xspeed = -xspeed;
     color = `#${getRandomColor()}`;
   }
   if (y + radius >= canva.height) {
+    // y = canva.height - radius;
     yspeed = -yspeed;
     color = `#${getRandomColor()}`;
   }
   if (x - radius <= 0) {
+    // x = canva.width - radius;
+    // x = 0 + radius;
     xspeed = -xspeed;
     color = `#${getRandomColor()}`;
   }
   if (y - radius <= 0) {
+    // y = 0 + radius;
     yspeed = -yspeed;
     color = `#${getRandomColor()}`;
   }
